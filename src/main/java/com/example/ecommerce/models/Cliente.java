fe79b0ec557bcb275786d79443bc5c0993632961
JorgeFVSz <jorge.silvestre@zup.com.br>
2025-01-30 11:01:59 -0300

    feat: add customer model

---
+++ src/main/java/com/example/ecommerce/models/Cliente.java
@@ -0,0 +1,51 @@
+package com.example.ecommerce.models;
+
+public class Cliente {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nome, String cpf, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
